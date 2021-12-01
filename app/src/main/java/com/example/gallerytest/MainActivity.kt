7cce4598d0a6960424b3f3b1cfb6c0fd85ce0114
hoshino_ken <ken3.2000@gmail.com>
2021-12-01 12:00:54 +0900

    add share function

--- app/src/main/java/com/example/gallerytest/MainActivity.kt
+++ app/src/main/java/com/example/gallerytest/MainActivity.kt
@@ -2,21 +2,26 @@ package com.example.gallerytest
 
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    //画像のUri
+    private var imgUri:String=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var pictureButton : Button = findViewById(R.id.button)
 
-        //ボタンのイベントリスナー
+        //画像選択ボタンのイベントリスナー
         pictureButton.setOnClickListener {
             val intent=Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
                 addCategory(Intent.CATEGORY_OPENABLE)
@@ -24,6 +29,24 @@ class MainActivity : AppCompatActivity() {
             }
             startActivityForResult(intent, READ_REQUEST_CODE)
         }
+
+        var shareButton : Button = findViewById(R.id.button2)
+        //シェアボタンのイベントリスナー
+        shareButton.setOnClickListener {
+            openChooserToShare("test string",Uri.parse(imgUri))
+        }
+
+    }
+
+    private fun openChooserToShare(str:String,img:Uri){
+        val sendIntent = Intent().apply {
+            action=Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT,str)
+            putExtra(Intent.EXTRA_STREAM,img)
+            type="*/*"
+        }
+        val shareIntent=Intent.createChooser(sendIntent,null)
+        startActivity(shareIntent)
     }
 
     companion object{
@@ -40,10 +63,12 @@ class MainActivity : AppCompatActivity() {
             READ_REQUEST_CODE->{
                 try{
                     resultData?.data?.also {uri->
+                        imgUri=uri.toString()
                         val inputStream=contentResolver?.openInputStream(uri)
                         val image=BitmapFactory.decodeStream(inputStream)
                         val imageView=findViewById<ImageView>(R.id.imageView)
                         imageView.setImageBitmap(image)
+                        //ここでAIにBitmapの情報を流す
                     }
                 }catch (e:Exception){
                     Toast.makeText(this,"error was occurred",Toast.LENGTH_LONG).show()
